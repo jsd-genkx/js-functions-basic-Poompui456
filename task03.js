8d4bd8e3fa617ed54ab01fc6e0918980f2b89f3a
Siriporn Worranawin <cheenoi30@gmail.com>
2025-07-16 09:44:32 +0700

    task03 solutions

--- task03.js
+++ task03.js
@@ -15,11 +15,15 @@ Task 03: FizzBuzz Checker
 ❌ Do not print inside the function.
 ✅ Only return the result.
 */
-
-// 👨‍💻 TODO-01: Write your code below to complete the task as described above.
-
 // Example Test Cases
 console.log(fizzBuzz(15)); // "FizzBuzz"
 console.log(fizzBuzz(9)); // "Fizz"
 console.log(fizzBuzz(10)); // "Buzz"
 console.log(fizzBuzz(7)); // "Not divisible by 3 or 5"
+// 👨‍💻 TODO-01: Write your code below to complete the task as described above.
+function fizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {return "FizzBuzz";
+  } else if (number % 3 === 0) {return "Fizz";
+  } else if (number % 5 === 0) {return "Buzz";}
+  else {return "Not divisible by 3 or 5";}
+}
